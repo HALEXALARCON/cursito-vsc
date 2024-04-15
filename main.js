3d5d32e9e7e14216158ccc1fa6a592365ae5ff79
ALEXCON <alexalarcon967@gmail.com>
2024-04-15 12:23:33 -0500

    mejorando

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-console.log("mejorando cada dia");console.log("hay nuevos cambios");
-let num1 = 20;
-let num2 = 30;
-let res = num1 + num2;
-control.log(res);
+
+let nombre = lucas;
+let saludo = "hola" +  nombre;
+console.log(saludo);
